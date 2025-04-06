22606076efeabb49a27c98c752cef43a1854c8f4
Calderon0723 <00215818@uca.edu.sv>
2025-04-06 16:52:00 -0600

    ADD: Clase Gestor Discos :sparkles:

---
+++ src/Classes/GestorDisco.java
@@ -0,0 +1,8 @@
+package Classes;
+
+import java.util.List;
+
+public class GestorDisco {
+    private List<Disco> Discos;
+    
+}
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import Classes.Disco;
+
 public class Main {
     public static void main(String[] args) {
+        Disco disco1 = new Disco(1, "Disco 1", 10);
 
+        disco1.InfoDisco();
     }
 }
